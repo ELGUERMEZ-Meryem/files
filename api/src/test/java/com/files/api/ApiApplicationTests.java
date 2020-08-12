c325c97c4ae7a976a8192e0e160501aab794f2a7
El Guermez Meryem <mac@SBAA.local>
2020-08-12 18:53:35 +0100

    add api and client

---
+++ api/src/test/java/com/files/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.files.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
